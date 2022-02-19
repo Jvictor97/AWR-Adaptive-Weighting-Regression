f359f1effa759f907644b296be106672d7c2a818
Joao Souza <jvictor.9@hotmail.com>
2022-02-18 23:39:58 -0300

    adds prepare_joint and predictions output

--- predict.py
+++ predict.py
@@ -12,6 +12,7 @@ from model.resnet_deconv import get_deconv_net
 from model.hourglass import PoseNet
 from model.loss import My_SmoothL1Loss
 from dataloader.custom_loader import CustomLoader
+from dataloader.nyu_loader import NYU
 from util.feature_tool import FeatureModule
 from util.eval_tool import EvalUtil
 from util.vis_tool import VisualUtil
@@ -45,7 +46,11 @@ class Trainer(object):
             print(pth['best_records'])
         self.net = self.net.cuda()
 
-        self.testData = CustomLoader(self.data_dir, 'test', img_size=self.config.img_size)
+        if self.config.dataset == 'nyu':
+          self.testData = NYU(self.data_dir, 'test', img_size=self.config.img_size, cube=self.config.cube)
+        else:
+          self.testData = CustomLoader(self.data_dir, 'test', img_size=self.config.img_size)
+
         self.criterion = My_SmoothL1Loss().cuda()
 
         self.FM = FeatureModule()
@@ -57,7 +62,7 @@ class Trainer(object):
 
         eval_tool = EvalUtil(self.testData.img_size, self.testData.paras, self.testData.flip, self.testData.jt_num)
         joint_predictions = []
-        for ii, (img, M, cube) in tqdm(enumerate(self.testLoader)):
+        for ii, (img, center_uvd, M, cube) in tqdm(enumerate(self.testLoader)):
 
             input = img.cuda()
 
@@ -69,7 +74,8 @@ class Trainer(object):
             cube = cube.detach().numpy()
             jt_uvd_pred = jt_uvd_pred.detach().cpu().numpy()
             for i in range(jt_uvd_pred.shape[0]):
-              joint_predictions.append(jt_uvd_pred[i])
+              pred = prepare_joint_prediction(jt_uvd_pred[i], center_uvd[i], M[i], cube[i], self.config.img_size)
+              joint_predictions.append(pred)
 
         print('FINISHED!')
 
@@ -78,6 +84,21 @@ class Trainer(object):
             jt_uvd = np.array(joint_predictions, dtype = np.float32)
             if not txt_file == None:
                 np.savetxt(txt_file, jt_uvd.reshape([jt_uvd.shape[0], self.config.jt_num * 3]), fmt='%.3f')
+
+def prepare_joint_prediction(jt_uvd_pred, center_uvd, M, cube, img_size):
+  jt_uvd_pred = np.squeeze(jt_uvd_pred).astype(np.float32)
+  center_uvd = np.squeeze(center_uvd).astype(np.float32)
+  M = np.squeeze(M).astype(np.float32)
+  cube = np.squeeze(cube).astype(np.float32)
+
+  M_inv = np.linalg.inv(M)
+
+  jt_uvd_pred[:, :2] = (jt_uvd_pred[:, :2] + 1) * img_size / 2.
+  jt_uvd_pred[:, 2] = jt_uvd_pred[:, 2] * cube[2] / 2. + center_uvd[2]
+  jt_uvd_trans = np.hstack([jt_uvd_pred[:, :2], np.ones((jt_uvd_pred.shape[0], 1))])
+  jt_uvd_pred[:, :2] = np.dot(M_inv, jt_uvd_trans.T).T[:, :2]
+
+  return jt_uvd_pred
 
 if __name__=='__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = '0'
