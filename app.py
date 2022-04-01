caa85448cc6f55948e255dc707a4bf945797ffd7
Joao Souza <jvictor.9@hotmail.com>
2022-04-01 11:56:52 -0300

    Implementa servidor flask para predição

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, request
 from single_predict import Predict
 from config import opt
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/calculate-joint-coordinates", methods=['POST'])
-# def calculate_joint_coordinates():
-#   body = request.json
+predict = Predict(opt)
 
-#   return { 'foo': 123 }, 201
+@app.route("/calculate-joint-coordinates", methods=['POST'])
+def calculate_joint_coordinates():
+  frame = request.files['frame']
+  centroid = request.json['centroid']
 
-p1 = Predict(opt, 'http')
-p1.set_frame([[]], [1,2,3])
+  predict.set_frame(frame, centroid)
+  prediction = predict.execute()
 
-print(p1)
+  return { 'prediction': prediction }
+		
+if __name__ == '__main__':
+   app.run(debug = True)
